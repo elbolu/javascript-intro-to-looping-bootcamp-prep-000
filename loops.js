a25bafc561316b3e22025fc53a198a3dd6e789e8
Leah Hill <leah@verysandwich.com>
2018-08-07 00:19:30 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,10 +1,5 @@
-function makeArray() {
-  const array = []
-  const t = Math.floor(Math.random() * 10)
-
-  for (let i = 0; i < 25; i++) {
+for (let i = 0; i < 25; i++) {
     array.push("I am a strange loop.")
   }
 
   return [array, 25]
-}
